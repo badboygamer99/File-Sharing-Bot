26e67cdd5bbf657562120be1f5eb9abd1ba3d6cf
badboygamer99 <141706370+badboygamer99@users.noreply.github.com>
2023-08-09 19:33:01 +0530

    cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='https://t.me/zoromoviehub'>Zoro Movie Hub</a>\n○ Language : English and Hindi○ Channel : @zoromoviehub\n○",
+            text = f"<b>○ Creator : <a href='https://t.me/zoromoviehub'>Zoro Movie Hub</a>\n○ Language : English and Hindi○ Channel : @zoromoviehub</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
